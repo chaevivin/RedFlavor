442fddfb40085d8bcb68cf1f11ae76fed89e3cd0
Chaevivin <chaevivin@gmail.com>
2023-11-25 21:35:30 +0900

    router 확인

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+import Intro from './pages/Intro';
+import Main from './pages/Main';
+import { createBrowserRouter } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
+import Profile from './pages/Profile';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    children: [
+      { index: true, element: <Intro /> },
+      { path: 'main', element: <Main /> },
+      { path: 'profile', element: <Profile /> }
+    ]
+  }
+])
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Intro.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
+export default function Intro() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate("main");
+    }, 2000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Intro</h1>
+    </div>
+  );
+}
+
