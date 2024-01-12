d2e5840598b9ed16c0a9ccb80c1a3bc426db7653
Chaevivin <chaevivin@gmail.com>
2024-01-12 16:44:58 +0900

    Style: Intro 페이지 스타일링

--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
+import styles from './Intro.module.css';
 
 export default function Intro() {
   const navigate = useNavigate();
+  const imgRef = useRef<HTMLImageElement | null>(null);
+  const [count, setCount] = useState(1);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
+    const timer = setInterval(() => {
+      setCount((prev) => prev + 1);
+      console.log(count);
+        if (imgRef.current) {
+          imgRef.current.src = `/img/intro_${count}.jpg`;
+        }
+      }, 1000);
+
+    if (count === 4) {
+      clearInterval(timer);
       navigate("main");
-    }, 2000);
-    return () => clearTimeout(timer);
-  }, []);
+    }
+
+    return () => clearInterval(timer);
+  }, [count, navigate]);
 
   return (
-    <div>
-      <h1>Intro</h1>
-    </div>
+    <article className={styles.container}>
+      <img ref={imgRef} alt='Intro Background' src='/img/intro_1.jpg' className={styles.background} />
+    </article>
   );
 }
 
