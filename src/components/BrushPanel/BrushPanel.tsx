93339220280d0b0c92d38e2a337da7bf3d846fba
Chaevivin <chaevivin@gmail.com>
2024-01-27 02:54:50 +0900

    Chore: BrushPanel import 오류 수정

--- src/components/BrushPanel/BrushPanel.tsx
+++ src/components/BrushPanel/BrushPanel.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import BrushType from '../BrushType/BrushType';
-import BrushSize from './../BrushSize/BrushSize';
 import styled from 'styled-components';
 import UndoRedo from '../UndoRedo/UndoRedo';
+import BrushSize from '../BrushSize/BrushSize';
 
 interface BrushPanelProps {
   fabricCanvasRef: React.MutableRefObject<fabric.Canvas | null>;
