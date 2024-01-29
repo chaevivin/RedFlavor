b5c1b591d64f085587be61be8f1bdb0cc9e1cfca
Chaevivin <chaevivin@gmail.com>
2024-01-29 21:09:12 +0900

    Style: Main 페이지 CSS 수정

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -9,26 +9,15 @@ const Background = styled.section<{ $imgurl: string | undefined }>`
   background-image: url(${p => p.$imgurl});
   background-size: cover;
   background-repeat: no-repeat;
-  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `
 
 const Logo = styled.img`
-  width: 247px;
+  width: calc(738px / 3);
   margin-bottom: 1rem;
-`
-
-const Container = styled.div`
-	position: absolute;
-  top: 27%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-`
-
-const Buttons = styled.div`
-  position: absolute;
-  top: 64%;
-  left: 52%;
-  transform: translate(-50%, -47%);
 `
 
 const ButtonContainer = styled.div`
@@ -37,10 +26,8 @@ const ButtonContainer = styled.div`
 `
 
 const CheckBox = styled.img`
-  width: calc(65px / 3);
-  height: calc(67px / 3);
-  position: absolute;
-  left: -2.7rem;
+  width: calc(67px / 3);
+  margin-right: 1.3rem;
 `
 
 const LinkButton = React.memo(styled(Link)<{ $imgurl: string | undefined; $playlist: boolean }>`
@@ -88,27 +75,23 @@ export default function Main() {
     <>
       {mainList &&
         <Background $imgurl={mainList[0]}>
-          <Container>
-            <Logo src={mainList[7]}></Logo>
-          </Container>
-          <Buttons>
-            <ButtonContainer>
-              <CheckBox src={mainList[6]}></CheckBox>
-              <LinkButton $imgurl={mainList[2]} $playlist={false} to='/profile'>프로필 보기</LinkButton>
-            </ButtonContainer>
-            <ButtonContainer>
-              <CheckBox src={mainList[6]}></CheckBox>
-              <LinkButton $imgurl={mainList[3]} $playlist={false} to='/photocard'>포토카드 꾸미기</LinkButton>
-            </ButtonContainer>
-            <ButtonContainer>
-              <CheckBox src={mainList[6]}></CheckBox>
-              <LinkButton $imgurl={mainList[4]} $playlist={false} to='/loading'>마이룸</LinkButton>
-            </ButtonContainer>
-            <ButtonContainer>
-              <CheckBox src={mainList[6]}></CheckBox>
-              <LinkButton $imgurl={mainList[5]} $playlist={true} to='/playlist'>playlist</LinkButton>
-            </ButtonContainer>
-          </Buttons>
+          <Logo src={mainList[7]}></Logo>
+          <ButtonContainer>
+            <CheckBox src={mainList[6]}></CheckBox>
+            <LinkButton $imgurl={mainList[2]} $playlist={false} to='/profile'>프로필 보기</LinkButton>
+          </ButtonContainer>
+          <ButtonContainer>
+            <CheckBox src={mainList[6]}></CheckBox>
+            <LinkButton $imgurl={mainList[3]} $playlist={false} to='/photocard'>포토카드 꾸미기</LinkButton>
+          </ButtonContainer>
+          <ButtonContainer>
+            <CheckBox src={mainList[6]}></CheckBox>
+            <LinkButton $imgurl={mainList[4]} $playlist={false} to='/loading'>마이룸</LinkButton>
+          </ButtonContainer>
+          <ButtonContainer>
+            <CheckBox src={mainList[6]}></CheckBox>
+            <LinkButton $imgurl={mainList[5]} $playlist={true} to='/playlist'>playlist</LinkButton>
+          </ButtonContainer>
         </Background>
       }
     </>
