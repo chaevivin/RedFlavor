937f7021e50069c376aa947291fcaf9078588526
Chaevivin <chaevivin@gmail.com>
2024-01-30 23:46:05 +0900

    Style: Myroom 캐릭터 크기 키움

--- src/components/Character/Character.tsx
+++ src/components/Character/Character.tsx
@@ -8,7 +8,7 @@ import styled from 'styled-components';
 
 const CharacterButton = styled.button`
   padding: 0;
-  width: calc(100% / 4.9);
+  width: calc(100% / 4.5);
   background-color: transparent;
   border: none;
   position: absolute;
