cd65771bde01562e7462f8c3d89c0dd10d919c2a
Chaevivin <chaevivin@gmail.com>
2024-02-03 15:01:11 +0900

    Feat: Firebase Performance Monitoring 추가

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
+import { getPerformance } from "firebase/performance";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -18,3 +19,4 @@ const app = initializeApp(firebaseConfig);
 
 export const storage = getStorage(app);
 export const database = getFirestore(app);
+export const perf = getPerformance(app);
